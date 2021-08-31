0b5c73b7f048c1a58d744660499009da25082824
Jay Pinion <jay@stormideas.com>
2021-08-31 14:41:20 +0100

    Finished Gatsby tutorial Part 2

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Layout from "../components/layout";
+
+const AboutPage = () => {
+  return (
+    <main>
+      <Layout pageTitle="About Me">
+        <p>Hi there! I'm Jay. I'm a Software Developer. </p>
+      </Layout>
+    </main>
+  );
+};
+
+export default AboutPage;
