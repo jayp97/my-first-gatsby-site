53109101541afedffe2071157ffaf47f400c70f3
Jay Pinion <jay@stormideas.com>
2021-08-31 18:04:54 +0100

    Finished Gatsby Tutorial Part 4

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,17 @@
 import React from "react";
 import Layout from "../components/layout";
+import { StaticImage } from "gatsby-plugin-image";
 
 const IndexPage = () => {
   return (
-    <main>
-      <Layout pageTitle="Home Page">
-        <p>I'm making this by following the gatsby tutorial!</p>
-      </Layout>
-    </main>
+    <Layout pageTitle="Home Page">
+      <p>I'm making this by following the gatsby tutorial!</p>
+      <StaticImage
+        alt="Crazy duck looking crazy!"
+        src="https://i.pinimg.com/236x/0b/c0/c5/0bc0c5ee2e27506dc1f1e03cba3f9636.jpg"
+        // src="../images/crazy-duck.jpg"
+      />
+    </Layout>
   );
 };
 
